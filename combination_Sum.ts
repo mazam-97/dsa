b631137e448cac28132477c53cb5d2b6c5374d11
mazam-97 <azam.dilshad@gmail.com>
2026-06-24 19:23:26 +0530

    combination_sum

---
+++ combination_Sum.ts
@@ -0,0 +1,37 @@
+function combination_sum  (candidates: number[], target: number) {
+
+  let outputs:number[][] = [];
+
+  backtrack(0,[],0);
+  return outputs;
+  function backtrack(index: number, currentSet: number[],sum:number){
+  
+    if(sum === target){
+      outputs.push([...currentSet]);
+      return;
+
+    }
+    if(index >= candidates.length || sum > target ){
+      return;
+    }
+
+    currentSet.push(candidates[index] as number);
+
+    backtrack(index, currentSet, sum + candidates[index as any] as any);
+
+    currentSet.pop();
+
+    backtrack(index+1, currentSet, sum);
+
+    return;
+
+
+
+
+  }
+
+}
+
+console.log(combination_sum([2,3,4,5],8));
+
+
